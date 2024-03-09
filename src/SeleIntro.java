d31eb8bdec86ecee943187042ef660f4ecb62a39
Burnett <ryburnett77@gmail.com>
2024-03-09 16:21:54 -0500

    initial commit

---
+++ src/SeleIntro.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleIntro {
+
+	public static void main(String[] args) {
+		
+		//Invoking Browser
+		//Chrome - ChromeDriver -> Use Methods -> Close Browser
+		//ChromeDriver driver = new ChromeDriver(); both work but ChromeDriver can only invoke chrome methods not firefox/safari
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.close();
+
+	}
+
+}
