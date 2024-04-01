b3ee3ed53e5e9e53d4b60b1bab291ff2c76dc874
unknown <ryburnett77@gmail.com>
2024-04-01 01:19:28 -0400

    selenium basics

--- src/SeleIntro.java
+++ src/SeleIntro.java
@@ -1,5 +1,9 @@
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleIntro {
 
@@ -12,16 +16,22 @@ public class SeleIntro {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://rahulshettyacademy.com/");
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
 		
-		System.out.println(driver.getTitle());
+		System.setProperty("webdriver.edge.driver", "C:\\edgedriver\\msedgedriver.exe");
+		//WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/");
+				
+		System.out.println(driver.getTitle()); 
 		
 		System.out.println(driver.getCurrentUrl());
+	
 		
 		driver.close(); //closes out of original tab (if new ones are opened it won't close out)
 		
-		driver.quit(); //quits entire window/browser sesh
-		
+		//driver.quit(); //quits entire window/browser sesh (don't use with firefox?)
 		
 		
 		
